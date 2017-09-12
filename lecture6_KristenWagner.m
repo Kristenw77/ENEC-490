f3b599c0710aa3d1c087106988197c91fec897b3
Kristen Wagner <31291622+Kristenw77@users.noreply.github.com>
2017-09-12 13:49:09 -0400

    Update lecture6_KristenWagner.m
    We worked on this for a couple hours and came up with this, a triple for loop that has the goal of assigning values to a matrix. The only issue is that we can't assign a whole list of numbers to a single cell. We thought about potentially using the IDX thing but weren't sure how we'd do that without using kmeans. Help!!!

--- lecture6_KristenWagner.m
+++ lecture6_KristenWagner.m
@@ -1,5 +1,18 @@
-[rows columns] = size(jan);
-[rows2 columns2] = size(july);
-weekdays = zeros(length(rows),1);
-weekdays2 = zeros(lenght(rows2),1);
-week = 7;
+colVector = mat2vec(data_2014);
+weekday = zeros(length(colVector)/24,2)
+   % Wednesday = days2(1:24) 
+   % Thursday = days2(25:49) 
+   % Friday = days2(50:74)
+    %Saturday = days2(75:99)
+    %Sunday = days2(100:124)
+   % Monday = days2(125:149)
+   % Tuesday = days2(150:174) 
+for i = 1:colVector
+    for j = 1:2
+    weekday(i,1) = colVector(i:i + 24)
+    i = i + 24
+        for k = 1:7
+            weekday(i, 2) = k
+        end 
+    end 
+end 
